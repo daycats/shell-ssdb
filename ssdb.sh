97e8533788a5dddaef11bd272f59a1814d18fe0e
Shanli Wei <myweishanli@gmail.com>
2016-07-06 09:14:19 +0800

    Create ssdb.sh
    init

---
+++ ssdb.sh
@@ -0,0 +1,14 @@
+
+# ssdb install
+yum -y install wget unzip gcc gcc-c++ autoconf
+
+cd /tmp
+wget --no-check-certificate https://github.com/ideawu/ssdb/archive/master.zip  -O ssdb.zip
+unzip ssdb.zip
+cd ssdb-master/deps/jemalloc-4.1.0/
+./autogen.sh && make
+cd ../../
+make && make install
+/usr/local/ssdb/ssdb-server -d /usr/local/ssdb/ssdb.conf
+echo "/usr/local/ssdb/ssdb-server -d -s restart /usr/local/ssdb/ssdb.conf" >> /etc/rc.local
+cd /tmp
